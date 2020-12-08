35621a91100c8eabfea9f5ebad34b35c81cf9ec8
Vinicius Teixeira <vinhast@gmail.com>
2020-12-08 20:59:02 -0300

    Initial commit

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,42 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    
+    response.json({
+      transactions,
+      balance
+    });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ 
+      error: err.message 
+    });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    );
+
+    const transition = createTransaction.execute({
+      title,
+      type,
+      value
+    });
+
+    return response.json(transition);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
